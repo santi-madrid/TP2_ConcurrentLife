1d8508295a361686477fd690e601d03c4f59f5c7
Ignacio Ledesma <ignacioledesmachiappero@gmail.com>
2026-04-09 18:47:28 -0300

    feat: implement CL_Queue class for managing waiting threads and transitions

---
+++ new/src/CL_Queue/CL_Queue.java
@@ -0,0 +1,47 @@
+package CL_Queue;
+
+import config.PetriNetConfig;
+import java.util.concurrent.Semaphore;
+
+public class CL_Queue {
+
+  /* Constantes de clase */
+  private static final int TRANSITIONS = PetriNetConfig.TRANSITIONS;
+
+  private Semaphore[] waitingThreads;
+  private int[] waitingTransitions;
+
+  public CL_Queue() {
+    this.waitingThreads = new Semaphore[TRANSITIONS];
+    this.waitingTransitions = new int[TRANSITIONS];
+
+    for (int i = 0; i < TRANSITIONS; i++) {
+      this.waitingThreads[i] = new Semaphore(0, true);
+    }
+  }
+
+  public boolean hasWaitingThreads() {
+    for (int i = 0; i < TRANSITIONS; i++) {
+      if (waitingThreads[i].hasQueuedThreads()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public int[] getWaitingTransitions() {
+    return waitingTransitions;
+  }
+
+  public void acquireTransition(int transition) throws InterruptedException {
+    waitingTransitions[transition] = 1; // Marcamos que hay un hilo esperando por esta transición.
+    waitingThreads[transition].acquire();
+  }
+
+  public void releaseTransition(int transition) {
+    if (waitingTransitions[transition] == 1) {
+      waitingTransitions[transition] = 0;
+      waitingThreads[transition].release();
+    }
+  }
+}
