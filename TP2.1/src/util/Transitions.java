d2f87c34caafd38ee795ec658a19efd18868d653
Ignacio Ledesma <ignacioledesmachiappero@gmail.com>
2026-04-09 17:31:09 -0300

    chore: notes and feedback for RdP and Transitions classes

--- TP2.1/src/util/Transitions.java
+++ TP2.1/src/util/Transitions.java
@@ -1,49 +1,52 @@
 package util;
+
 import java.util.*;
 
+// * Me gusta que haya un archivo solamente para metodos de transiciones
+// * Mis comentarios sobre los metodos de aca ya estan en la clase RdP
 public class Transitions {
-    String name;
-    List<Places> inputPlaces;
-    List<Places> outputPlaces;
+  String name;
+  List<Places> inputPlaces;
+  List<Places> outputPlaces;
 
-    Transitions(String name, List<Places> inputPlaces, List<Places> outputPlaces) {
-        this.name = name;
-        this.inputPlaces = inputPlaces;
-        this.outputPlaces = outputPlaces;
-    }
+  Transitions(String name, List<Places> inputPlaces, List<Places> outputPlaces) {
+    this.name = name;
+    this.inputPlaces = inputPlaces;
+    this.outputPlaces = outputPlaces;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public List<Places> getInputPlaces() {
-        return inputPlaces;
-    }
+  public List<Places> getInputPlaces() {
+    return inputPlaces;
+  }
 
-    public List<Places> getOutputPlaces() {
-        return outputPlaces;
-    }
+  public List<Places> getOutputPlaces() {
+    return outputPlaces;
+  }
 
-    public boolean isEnabled() {
-        for (Places place : inputPlaces) {
-            if (place.getTokens() == 0) {
-                return false;
-            }
-        }
-        return true;
+  public boolean isEnabled() {
+    for (Places place : inputPlaces) {
+      if (place.getTokens() == 0) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    public void fire() {
-        if (isEnabled()) {
-            for (Places place : inputPlaces) {
-                place.removeTokens(1);
-            }
-            for (Places place : outputPlaces) {
-                place.addTokens(1);
-            }
-            System.out.println("Transition " + name + " fired.");
-        } else {
-            System.out.println("Transition " + name + " cannot be fired.");
-        }
+  public void fire() {
+    if (isEnabled()) {
+      for (Places place : inputPlaces) {
+        place.removeTokens(1);
+      }
+      for (Places place : outputPlaces) {
+        place.addTokens(1);
+      }
+      System.out.println("Transition " + name + " fired.");
+    } else {
+      System.out.println("Transition " + name + " cannot be fired.");
     }
+  }
 }
